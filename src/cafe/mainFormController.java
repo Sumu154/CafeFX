6b5c954f8912d458aab5b8fb2d4388d6132de478
araf <muntasiraraf12@gmail.com>
2024-03-25 16:16:37 +0600

    Fourth commit

---
+++ src/cafe/mainFormController.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cafe;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+
+/**
+ *
+ * @author sumaiya, saif, araf
+ */
+
+public class mainFormController implements Initializable {
+    
+    @FXML
+    private Button customers_btn;
+
+    @FXML
+    private Button dashboard_btn;
+
+    @FXML
+    private Button inventory_addBtn;
+
+    @FXML
+    private Button inventory_btn;
+
+    @FXML
+    private Button inventory_clearBtn;
+
+    @FXML
+    private TableColumn<productData, String> inventory_col_date;
+    
+    @FXML
+    private TableColumn<productData, String> inventory_col_price;
+
+    @FXML
+    private TableColumn<productData, String> inventory_col_productID;
+
+    @FXML
+    private TableColumn<productData, String> inventory_col_productName;
+
+    @FXML
+    private TableColumn<productData, String> inventory_col_status;
+
+    @FXML
+    private TableColumn<productData, String> inventory_col_stock;
+
+    @FXML
+    private TableColumn<productData, String> inventory_col_type;
+
+    @FXML
+    private Button inventory_deleteBtn;
+
+    @FXML
+    private AnchorPane inventory_form;
+
+    @FXML
+    private ImageView inventory_imageView;
+
+    @FXML
+    private Button inventory_importBtn;
+
+    @FXML
+    private TextField inventory_price;
+
+    @FXML
+    private TextField inventory_productID;
+
+    @FXML
+    private TextField inventory_productName;
+
+    @FXML
+    private ComboBox<?> inventory_status;
+
+    @FXML
+    private TextField inventory_stock;
+
+    @FXML
+    private TableView<productData> inventory_tableView;
+
+    @FXML
+    private ComboBox<?> inventory_type;
+
+    @FXML
+    private Button inventory_updateBtn;
+
+    @FXML
+    private Button logout_btn;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private Button menu_btn;
+    
+    
+    
+    
+    
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        inventoryTypeList();
+        inventoryStatusList();
+        inventoryShowData();
+    }
+    
+}
